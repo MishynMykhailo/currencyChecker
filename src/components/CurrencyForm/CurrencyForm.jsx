69694e21495e6f30c341527a43e1832913ed25fa
MishynMykhailo <misham18092001@gmail.com>
2022-10-12 22:26:58 +0300

    update

--- src/components/CurrencyForm/CurrencyForm.jsx
+++ src/components/CurrencyForm/CurrencyForm.jsx
@@ -1,7 +1,6 @@
 import s from './CurrencyForm.module.css';
 import { useState } from 'react';
 
-import CurrencySelect from 'components/CurrencySelect';
 import currencies from '../../service/currenciesID.json';
 import Loader from 'components/Loader';
 // 0.0061637
@@ -14,39 +13,6 @@ const CurrencyForm = ({
   COMMISSION,
 }) => {
   const [currenciesArr] = useState(currencies);
-  const [filterFrom, setFilterFrom] = useState('');
-  const [filterTo, setFilterTo] = useState('');
-
-  //Записывает изменения в Filter,для фильтрации валют в Input
-  const handlerChangeInput = e => {
-    const { name, value } = e.currentTarget;
-    switch (name) {
-      case 'from':
-        setFilterFrom(value);
-        break;
-      case 'to':
-        setFilterTo(value);
-        break;
-      default:
-        return;
-    }
-  };
-
-  //Совместить в один метод !!!
-  const getVisibleCurrencyFrom = () => {
-    const normalizeFilterFrom = filterFrom.toLowerCase();
-    const normalizeArray = currenciesArr.filter(({ code }) => {
-      return code.toLowerCase().includes(normalizeFilterFrom);
-    });
-    return normalizeArray;
-  };
-
-  const getVisibleCurrencyTo = () => {
-    const normalizeFilterTo = filterTo.toLowerCase();
-    return currenciesArr.filter(currency =>
-      currency.code.toLowerCase().includes(normalizeFilterTo)
-    );
-  };
 
   return (
     <>
@@ -82,26 +48,6 @@ const CurrencyForm = ({
             Погрешность RUB/USD = +-0.10 - 0.12
           </span>
         )}
-
-        <div className={s.containerFormSelect}>
-          <CurrencySelect
-            name={'from'}
-            selectName={'selectFrom'}
-            onChangeHandler={e => handlerChangeInput(e)}
-            getVisibleCurrency={getVisibleCurrencyFrom}
-          />
-          <CurrencySelect
-            name={'to'}
-            selectName={'selectTo'}
-            onChangeHandler={handlerChangeInput}
-            getVisibleCurrency={getVisibleCurrencyTo}
-          />
-        </div>
-        <div className={s.divBtn}>
-          <button type="submit" className={s.btn}>
-            Submit
-          </button>
-        </div>
       </form>
     </>
   );
